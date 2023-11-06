59ab8605b900ae04a2498cd9285f7758635943ed
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-11-06 14:54:47 +0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <div classNameName="container-fluid fh5co_header_bg">
+                <div classNameName="container">
+                    <div classNameName="row">
+                        <div classNameName="col-12 fh5co_mediya_center"><a href="#" classNameName="color_fff fh5co_mediya_setting"><i
+                            classNameName="fa fa-clock-o"></i>&nbsp;&nbsp;&nbsp;Friday, 5 January 2018</a>
+                            <div classNameName="d-inline-block fh5co_trading_posotion_relative"><a href="#" classNameName="treding_btn">Trending</a>
+                                <div classNameName="fh5co_treding_position_absolute"></div>
+                            </div>
+                            <a href="#" classNameName="color_fff fh5co_mediya_setting">Instagram’s big redesign goes live with black-and-white app</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+   
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/components/NewsAggregator.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NewsAggregator.css'
+
+function NewsAggregator() {
+  return (
+    <div>
+        {/*  */}
+    </div>
+  )
+}
+
+export default NewsAggregator
